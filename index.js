96d76ae2208388f736ceabe4e7a4cf02a6b57a80
Udson Souza <udson.cruz@serget.com.br>
2020-11-03 12:16:17 -0300

    Ajustado para a ageração de QRCode

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
 var html = fs.readFileSync('./layout/teste.html', 'utf8');
+var qrcode = require('qrcode')
 
-html = html.replace("{{nome}}", "Udson Souza da Cruz")
+const code = qrcode.toDataURL("Eu sou um QRCode diferentao", function(err, value ) {
+  if (err) {
+    console.log(err)
+    return
+  }
 
-var options = { format: 'A4' };
+  html = html
+    .replace("{{nome}}", "Udson Souza da Cruz")
+    .replace("{{qrcode}}", value)
+  var options = { format: 'A4' };
+  
+  pdf.create(html, options).toFile('./teste.pdf', function(err, res) {
+    if (err) return console.log(err);
+    console.log(res); // { filename: '/app/businesscard.pdf' }
+  });
+})
 
-pdf.create(html, options).toFile('./teste.pdf', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
-});
+
 
